1b5c169cc55defdd840cd42d498525d3f7588dc2
MayceAlazzawi <maynab4@gmail.com>
2022-04-18 17:48:04 +0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // code your solution here
+
+const record = [
+  {year: "2018", result: "L"},
+  {year: "2015", result: "W"},
+  {year: "2016", result: "N/A"}
+]
+const superbowlWin = (arr) => {
+    arr.find(element => {
+     
+    if (element.result === "W"){
+      console.log(element.year)
+    } else {
+      console.log(undefined)
+    }
+  }
+  );
+}
+superbowlWin(record)
